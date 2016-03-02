885a1e45bbf2fe0b14763b3a17050589f9d656d3
jcahal <jacahal@gmail.com>
2016-03-02 00:31:35 -0700

    initial commit

---
+++ DC_MotorControl.ino
@@ -0,0 +1,27 @@
+/*
+ * Credit to Adafruit for this code. Thank you!
+ * 
+ */
+
+int motorPin = 3;
+ 
+void setup() 
+{ 
+  pinMode(motorPin, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      analogWrite(motorPin, speed);
+    }
+  }
+} 
